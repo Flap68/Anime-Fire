6e36a75af6e55e97fa835bc76013e37efc64bfb1
/Flap <78367609+Flap68@users.noreply.github.com>
2021-07-16 17:45:46 -0300

    atualização 9

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
-const imgs = document.getElementsByClassName("animdestaque");
-const img = document.querySelectorAll(".animdestaque img");
+const imgs = document.getElementById("img");
+const img = document.querySelectorAll("#img img");
 
 let idx = 0;
 
-function carrossel()(
-    idx++;
-    
-    if(idx > img.length -1){
-        idx = 0;
-    }
+		function carrossel() {
+			idx++;
 
-    imgs.styles.traansform = `translateX(${-idx * 500}px)`;
-)
+			if(idx > img.length - 1){
+				idx = 0;
+			}
 
-setInterval(carrossel, 1800);
+		
+			imgs.style.transform = `translateX(${-idx * 700}px)`;
+		}
+
+setInterval(carrossel, 3000);
+
+let show = true; 
+
+const menuSection = document.querySelector(".menu-section")
+const menuToggle = menuSection.querySelector(".menu-toggle")
+
+menuToggle.addEventListener("click", () => {
+
+    document.body.style.overflow = show ? "hidden" : "initial"
+
+    menuSection.classList.toggle("on", show)
+    show = !show;
+})
